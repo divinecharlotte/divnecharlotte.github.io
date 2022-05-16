9d7ca4d2a6273bf1798d30baff479b9694988d65
charlotte-divine <divinemaina@gmail.com>
2022-05-16 13:42:14 +0100

    mobile version

---
+++ app.js
@@ -0,0 +1,14 @@
+const navLinks = document.getElementById('navLinks');
+const burger = document.getElementById('burger');
+const hideMenu = document.getElementById('closeMenuIcon');
+const closeMenu = document.getElementById('navItems');
+burger.onclick = function burger() {
+  navLinks.style.left = '0';
+};
+hideMenu.onclick = function hideMenu() {
+  navLinks.style.left = '-100%';
+};
+
+closeMenu.onclick = function hideMenu() {
+  navLinks.style.left = '-100%';
+};
